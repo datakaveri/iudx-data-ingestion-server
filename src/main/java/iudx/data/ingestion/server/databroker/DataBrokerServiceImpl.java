1667275d29df5839ee71fb6878fd45a522d1260a
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2021-08-04 17:00:33 +0530

    Added Databroker service

---
+++ src/main/java/iudx/data/ingestion/server/databroker/DataBrokerServiceImpl.java
@@ -0,0 +1,20 @@
+package iudx.data.ingestion.server.databroker;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+
+public class DataBrokerServiceImpl implements DataBrokerService {
+
+	private static final Logger LOGGER = LogManager.getLogger(DataBrokerServiceImpl.class);
+
+	@Override
+	public DataBrokerService publishData(JsonObject request, Handler<AsyncResult<JsonObject>> handler) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
