9f4b1edec25cbea15a81bd7c01955a90337fb011
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2021-08-04 16:59:25 +0530

    Updated Databroker verticle

--- src/main/java/iudx/data/ingestion/server/databroker/DataBrokerVerticle.java
+++ src/main/java/iudx/data/ingestion/server/databroker/DataBrokerVerticle.java
@@ -1,5 +1,13 @@
 package iudx.data.ingestion.server.databroker;
 
-public class DataBrokerVerticle {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.vertx.core.AbstractVerticle;
+
+public class DataBrokerVerticle extends AbstractVerticle {
+
+	private static final String BROKER_SERVICE_ADDRESS = "iudx.rs.broker.service";
+	private static final Logger LOGGER = LogManager.getLogger(DataBrokerVerticle.class);
 
 }
